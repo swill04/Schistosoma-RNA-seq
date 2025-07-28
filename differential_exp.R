97fc5763903b4eb3d248cbcedf112c266de1825e
sophie <willetse0745@uwec.edu>
2025-07-28 15:43:28 -0500

    updates to diffexp

--- differential_exp.R
+++ differential_exp.R
@@ -1,139 +1,112 @@
 library(tidyverse)
 library(DESeq2)
 library(broom)
+library(ggrepel)
 
-star_counts_df <- read_tsv("/data/classes/2025/spring/biol443/course_files/rnaseq_data/counts.tsv", comment = "#") |>
+counts_df <- read_tsv("/data/users/willetse0745/Sm_Mira_IvT/pipeline/counts/star/counts.tsv", comment = "#") |>
              mutate(across(where(is.numeric), as.integer))
 
-star_counts_summary <- star_counts_df |>
-    select(Geneid, contains('dedup/star')) |>
-    rename_with(~str_remove(., "dedup/star/"), everything()) |>
-    rename_with(~str_remove(., "_S[0-9]{2}_L005.bam:.*"), everything()) |>
+counts_summary <- counts_df |>
+    select(Geneid, contains('bam')) |>
+    rename_with(~str_remove(., "dedup/star/.*.bam:"), everything()) |>
     rowwise() |>
     mutate(total_counts = sum(c_across(where(is.numeric)), na.rm = T)) |>
     filter(total_counts >= 10)
 
-star_sample_summary <- star_counts_df |>
-    select(Geneid, contains('dedup/star')) |>
-    rename_with(~str_remove(., "dedup/star/"), everything()) |>
-    rename_with(~str_remove(., "_S[0-9]{2}_L005.bam:.*"), everything()) |>
+sample_summary <- counts_df |>
+    select(Geneid, contains('bam')) |>
+    rename_with(~str_remove(., "dedup/star/.*.bam:"), everything()) |>
     pivot_longer(-Geneid, names_to = 'sample', values_to = 'count') |>
     filter(count > 0) |>
     group_by(Geneid) |>
     tally() |>
     filter(n <= 3)
 
-genes_to_remove = star_sample_summary$Geneid
+genes_to_remove = sample_summary$Geneid
 
-star_counts_filt <- star_counts_summary |>
+counts_filt <- counts_summary |>
     filter(!Geneid %in% genes_to_remove) |>
     arrange(Geneid) |>
     select(-total_counts)
 
-star_counts_m <- star_counts_filt |>
+counts_m <- counts_filt |>
     select(-Geneid) |>
     as.matrix()
-rownames(star_counts_m) <- star_counts_filt$Geneid
+rownames(counts_m) <- counts_filt$Geneid
 
-dists <- dist(t(star_counts_m))
-
-star_dists_df <- as.matrix(dists) |>
-    as_tibble(rownames = 'sample')
-
-star_dist_plot <- star_dists_df |>
-    pivot_longer(-sample, names_to = 'comp', values_to = 'dist') |>
-    ggplot(aes(x = sample, y = comp, fill = dist)) +
-    geom_tile() +
-    scale_fill_viridis_c() +
-    coord_equal() +
-    NULL
-
-star_pca_fit <- t(log10(star_counts_m + 1)) |> 
-  prcomp(scale = TRUE)
-
-star_pca_fit |>
-    augment(t(star_counts_m)) |>
-    dplyr::rename(sample = .rownames) |>
-    mutate(sample = str_remove(sample, '-[0-9]+')) |>
-    ggplot(aes(.fittedPC1, .fittedPC2, color = sample)) + 
-    geom_point(size = 5)
-
-star_metadata <- data.frame(sample_id = colnames(star_counts_m)) |>
+metadata <- data.frame(sample_id = colnames(counts_m)) |>
     mutate(tissue = str_sub(sample_id, 1, 3),
-           rep = str_sub(sample_id, 6))
-rownames(star_metadata) <- star_metadata$sample_id
-star_metadata <- select(star_metadata, -sample_id)
-star_metadata
+           rep = str_sub(sample_id, 6, 6))
+rownames(metadata) <- metadata$sample_id
+metadata <- select(metadata, -sample_id)
 
-all(rownames(star_metadata) == colnames(star_counts_m))
-
-dds <- DESeqDataSetFromMatrix(countData = star_counts_m,
-                              colData = star_metadata,
+dds <- DESeqDataSetFromMatrix(countData = counts_m,
+                              colData = metadata,
                               design = ~ tissue)
 dds <- DESeq(dds)
 
 res <- results(dds)
 
+vsd <- vst(dds)
+
+pca_fit <- t(assay(vsd)) |> 
+  prcomp(scale = TRUE)
+
+(pca_plot <- pca_fit |>
+  augment(t(assay(vsd))) |>
+  dplyr::rename(sample = .rownames) |>
+  separate(sample, into = c('tissue', 'rep'), sep = '-') |>
+    mutate(tissue = case_when(
+    tissue == 'Int' ~ 'Intestine',
+    tissue == 'Liv' ~ 'Liver'
+  )) |>
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, color = tissue, shape = tissue)) + 
+  geom_point(size = 4) +
+  scale_color_manual(values = c('indianred', 'steelblue')) +
+  labs(x = "PC1 (29% of variance)", y = "PC2 (23% of variance)", color = "Tissue source", shape = "Tissue source") +
+  theme_minimal() +
+  NULL
+)
+
+ggsave('plots/pca.pdf', pca_plot, width = 4, height = 4)
+ggsave('plots/pca.png', pca_plot, width = 4, height = 4)
+
 volcano_data <- as_tibble(res, rownames = 'gene_id')
+
+degs <- volcano_data |>
+    filter(log2FoldChange > 2 | log2FoldChange < -2,
+            -log10(padj) > -log10(0.05))
 
 volcano_plot <- volcano_data |> 
     mutate(color = case_when(
         log2FoldChange > 1  & padj < 0.05 | log2FoldChange < -1 & padj < 0.05 ~ 'deg',
-        TRUE ~ 'no deg'
+        TRUE ~ 'not deg'
     )) |> 
     ggplot(aes(x = log2FoldChange, y = -log10(padj))) +
     geom_point(aes(color = color),
         size = 5) +
+    geom_label_repel(data = degs,
+                     aes(x = log2FoldChange, y = -log10(padj), label = gene_id),
+                     max.overlaps = 50, size = 1) +
     geom_hline(yintercept = -log10(0.05)) +
     geom_vline(xintercept = 1) +
-    geom_vline(xintercept = -1) +
-    labs(
-        x = 'log2FoldChange',
-        y = '-log10(padj)'
-    ) + 
+    geom_vline(xintercept = -1) + 
     scale_color_manual(values = c('#EBB6B3', '#334139')) +
-    theme_minimal() + 
-    theme(
-        axis.title.x = element_text(size = 20),
-        axis.title.y = element_text(size = 20),
-        axis.text = element_text(size = 18),
-        legend.position = 'none'
-        )
+    theme_minimal()
 volcano_plot
 
-ggsave('plots/volcano.png', volcano_plot, width = 16, height = 10)
-ggsave("plots/volcano.pdf", volcano_plot, width = 16, height = 10)
+ggsave('plots/volcano.png', volcano_plot, bg = 'white', width = 6, height = 6)
+ggsave("plots/volcano.pdf", volcano_plot, bg = 'white', width = 6, height = 6)
 # caption on plot: All points are located on the bottom of the plot indicating genes that aren't significantlly differentially expressed.
 # caption ct'd: There are points that are located on the right/left - this indicates higher expression of that gene in the intestine vs liver.
 
-vsd <- varianceStabilizingTransformation(dds)
+norm_counts <- counts(dds, normalized = TRUE) |>
+    as_tibble(rownames = 'gene_id')
+write_csv(norm_counts, '/data/users/willetse0745/Sm_Mira_IvT/pipeline/deseq_results/deseq_norm_counts.csv')
 
-post_deseq2 <- plotPCA(vsd, intgroup = c("tissue"), returnData = TRUE) |>
-    as_tibble() |>
-    mutate(rep = str_extract(name, "[0-9]{2}")) |>
-    ggplot() +
-    geom_point(aes(x = PC1, y = PC2, color = group, shape = as.factor(rep)), 
-                size = 15) +
-    scale_shape_manual(values = c(15, 16, 17, 18)) +
-    scale_color_manual(values = c('indianred', 'steelblue'),
-                        labels = c('Intestine miracidia', 'Liver miracidia')) +
-    labs(color = 'Sample', shape = "Replicate") +
-    theme_minimal() +
-    theme(
-        axis.title.x = element_text(size = 20),
-        axis.title.y = element_text(size = 20),
-        axis.text = element_text(size = 18),
-        legend.title = element_text(size = 20),
-        legend.text = element_text(size = 18),
-        legend.position = 'bottom'
-        )
-post_deseq2
 
-ggsave('plots/post_deseq2.png', width = 16, height = 11)
-ggsave("plots/post_deseq2.pdf", width = 16, height = 11)
-# caption on plot: After performing deseq2 analysis, there is now separation of the points. 
-# caption ct'd: There are pretty clear clusters of a few points, with only a few being slightly removed from the others
 
+## from given normalized counts
 norm_counts <- read_csv('/data/classes/2025/spring/biol443/course_files/rnaseq_data/deseq_norm_counts.csv')
 
 norm_counts_long <- norm_counts |>
